044822f78720ddb760560b309d12b46a4e8ecd68
Oladele Idowu <olataurus15@gmail.com>
2022-02-23 02:24:45 +0100

    Initial commit for css

---
+++ js/greetingAssignment.js
@@ -0,0 +1,16 @@
+// program that will detect the dayparts Greeting on the console
+
+let dayParts = (15);
+let greet;
+
+    if (dayParts < 12) {
+        console.log(greet = 'Good Morning, You are welcome to the New Horizons');
+        }
+    
+    else if (dayParts >= 12 && dayParts <= 17) {
+        console.log(greet = 'Good Afternoon, You are welcome to the New Horizons');
+        }
+
+    else if (dayParts >= 17 && dayParts <= 24) {
+        console.log(greet = 'Good Evening, You are welcome to the New Horizons');
+        }
